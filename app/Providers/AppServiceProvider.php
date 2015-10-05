a97a2b154a53a2d3a7d4f3a58a65ebc6fc8c5bef
BePsvPT <freedom@csie.io>
2015-10-05 15:56:19 +0800

    add debugar and ide-helper packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (! $this->app->environment(['production'])) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
