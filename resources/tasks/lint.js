bd2429e0404134f46ddc426974370a663d45077d
DanSnow <dododavid006@gmail.com>
2015-10-23 14:21:32 +0800

    Fix lint task

--- resources/tasks/lint.js
+++ resources/tasks/lint.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-  gulp.src('./src/**/*.js')
+  gulp.src([
+      './resources/assets/**/*.js',
+      './resources/tasks/**/*.js',
+      './gulpfile.js'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
