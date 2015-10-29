e542f6ed068b4d7de59cd682f52a513bf16b82dd
BePsvPT <freedom@csie.io>
2015-10-29 22:56:50 +0800

    Update configs

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\User::class,
+        'model'  => App\Affair\User::class,
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
