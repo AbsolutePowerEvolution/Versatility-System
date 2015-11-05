c9bc9883f15838248f19c54f6e5a9a1b05fc57f0
DanSnow <dododavid006@gmail.com>
2015-11-05 14:21:55 +0800

    Add transform

--- resources/tasks/build.js
+++ resources/tasks/build.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
 gulp.task('build', ['lint'], function() {
-  browserify('./resources/assets/main.js')
+  browserify()
+    .add('./resources/assets/main.js', {debug: true})
+    .transform(babelify.configure({
+      presets: ['es2015'],
+      plugins: ['transform-es2015-modules-commonjs']
+    }))
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('public'));
