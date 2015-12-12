94c0e5e64682dec94bd8efc62e1e0837c6a5e5d8
DanSnow <dododavid006@gmail.com>
2015-12-13 00:35:09 +0800

    Auto start php server

--- resources/tasks/browser-sync.js
+++ resources/tasks/browser-sync.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var bg = require('gulp-bg');
 var browserSync = require('./helper/browser-sync');
 
-gulp.task('serve', ['build', 'watch'], function() {
+gulp.task('php-server', bg('php', 'artisan', 'serve'));
+
+gulp.task('proxy', ['build', 'watch', 'php-server'], function() {
   browserSync.init({
     proxy: {
       target: 'localhost:8000'
     }
   });
 });
+
+gulp.task('serve', ['proxy']);
