85a4a7430beb7c9c8a9403d669f21232d671e819
DanSnow <dododavid006@gmail.com>
2015-12-12 17:09:45 +0800

    Fix task; Rename output file

--- resources/tasks/sass.js
+++ resources/tasks/sass.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
-  return gulp.src('./resources/assets/app.scss')
+  return gulp.src('./resources/assets/sass/app.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass({ importer: require('./helper/sass-importer') }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/styles.css'));
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./public'));
 });
