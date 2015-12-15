dd45a08918f894b7cde90a9b673d9ce8a29a55fa
DanSnow <dododavid006@gmail.com>
2015-12-16 00:53:52 +0800

    Build all css; Add css min

--- resources/tasks/sass.js
+++ resources/tasks/sass.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var gulpif = require('gulp-if');
+var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
+var productionMode = process.env.NODE_ENV === 'production';
+
 gulp.task('sass', function() {
-  return gulp.src('./resources/assets/sass/app.scss')
+  return gulp.src('./resources/assets/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ importer: require('./helper/sass-importer') }))
     .pipe(sourcemaps.write())
-    .pipe(rename('styles.css'))
+    .pipe(concat('styles.css'))
+    .pipe(gulpif(productionMode, cssmin()))
+    .pipe(gulpif(productionMode, rename({suffix: '.min'})))
     .pipe(gulp.dest('./public'));
 });
