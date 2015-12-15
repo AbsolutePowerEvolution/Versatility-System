8be737d82cfcc1c2cf3fbfb76c19ed105a6fbd22
DanSnow <dododavid006@gmail.com>
2015-12-15 14:33:55 +0800

    Dynamic generate configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,66 @@ var webpack = require('webpack');
 var SplitByPathPlugin = require('webpack-split-by-path');
 var env = process.env.NODE_ENV || 'development';
 
-configs = {
-  entry: {
-    'bundle': './resources/assets/javascript/main.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: 'public'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'stage-0']
-      }
-    }, {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
-    }]
-  },
-  plugins: [
-    new SplitByPathPlugin([{
-      name: 'vendors',
-      path: path.join(__dirname, 'node_modules/')
-    }]),
-    new webpack.ProvidePlugin({
-      'jQuery': 'jquery',
-      '$': 'jquery'
-    })
-  ]
+var getDefaultConfigs = function() {
+  return {
+    entry: {
+      'bundle': './resources/assets/javascript/main.js'
+    },
+    output: {
+      filename: '[name].js',
+      path: 'public'
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }]
+    },
+    plugins: [
+      new SplitByPathPlugin([{
+        name: 'vendors',
+        path: path.join(__dirname, 'node_modules/')
+      }]),
+      new webpack.ProvidePlugin({
+        'jQuery': 'jquery',
+        '$': 'jquery'
+      })
+    ],
+  };
 };
 
-module.exports = configs;
-if(env === 'development' || env === 'production') {
-  configs.externals = [{
-    jquery: 'jQuery',
-    'materialize-css/dist/js/materialize': 'null',
-    sammy: 'Sammy',
-    mustache: 'Mustache'
-  }];
+var getConfigs = function(env) {
+  env = env || 'development';
 
-  if(env === 'production') {
-    configs.plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
-    );
+  configs = getDefaultConfigs();
+
+  if(env === 'development' || env === 'production') {
+    configs.externals = [{
+      jquery: 'jQuery',
+      'materialize-css/dist/js/materialize': 'null',
+      sammy: 'Sammy',
+      mustache: 'Mustache'
+    }];
+
+    if(env === 'production') {
+      configs.plugins.push(
+          new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              warnings: false
+            }
+          })
+      );
+    }
   }
-}
+  return configs;
+};
 
+module.exports = getConfigs(env);
+configs.getConfigs = getConfigs;
