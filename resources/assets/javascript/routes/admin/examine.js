9b1969faadacfd4f174acb256f7a5b2ac6910fac
DanSnow <dododavid006@gmail.com>
2015-12-19 00:56:06 +0800

    Add examine page prototype

---
+++ resources/assets/javascript/routes/admin/examine.js
@@ -0,0 +1,63 @@
+var Sammy = require('sammy');
+
+Sammy('#main', function() {
+  this.use('Mustache', 'ms');
+
+  this.get('#/admin/examine', function(context) {
+    console.log('examine');
+    context.list = [{
+      id: 1,
+      username: 'foo',
+      propertyName: 'class1',
+      time: '12:00'
+    }, {
+      id: 2,
+      username: 'bar',
+      propertyName: 'class2',
+      time: '10:00'
+    }];
+
+    context.partial('/templates/admin/examine.ms', context, () => {
+      // Content has been render
+      $('.Examine-Item').each((idx, ele) => {
+        var item = $(ele);
+        var id = item.data('id');
+
+        // Initialize tooltip
+        $('.tooltipped').tooltip({
+          delay: 50,
+          position: 'buttom'
+        });
+
+        // Re-trigger event for click
+        item.find('.Examine-Detail')
+          .click(() => item.trigger('examine-detail', id));
+
+        item.find('.Examine-Pass')
+          .click((event) => {
+            event.preventDefault();
+            item.trigger('examine-pass', id);
+          });
+        item.find('.Examine-Reject')
+          .click((event) => {
+            event.preventDefault();
+            item.trigger('examine-reject', id);
+          });
+
+        // Deal custom event
+        item.on('examine-detail', (event, id) => {
+          console.log(`Show detail for id: ${id}`);
+        });
+
+        item.on('examine-pass', (event, id) => {
+          console.log(`Examine pass id: ${id}`);
+        });
+
+        item.on('examine-reject', (event, id) => {
+          console.log(`Examine reject id: ${id}`);
+        });
+      });
+    });
+  });
+});
+
