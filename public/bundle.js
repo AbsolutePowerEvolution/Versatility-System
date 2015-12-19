88c97ea5773cf52d68bea5a6222cf8ef8caa2531
thumbd12856@gmail.com <thumbd12856@gmail.com>
2015-12-19 22:16:18 +0800

    merge bundle

--- public/bundle.js
+++ public/bundle.js
@@ -1,69 +1,77 @@
-webpackJsonp([0],[
-/* 0 */
+webpackJsonp([0],{
+
+/***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
 	__webpack_require__(1);
-	var Sammy = __webpack_require__(2);
-	__webpack_require__(3);
+	__webpack_require__(191);
+	var Sammy = __webpack_require__(192);
+	__webpack_require__(193);
 
-	__webpack_require__(6);
+	__webpack_require__(196);
 
 	Sammy('#main').run('#/');
 
 /***/ },
-/* 1 */
+
+/***/ 191:
 /***/ function(module, exports) {
 
 	module.exports = null;
 
 /***/ },
-/* 2 */
+
+/***/ 192:
 /***/ function(module, exports) {
 
 	module.exports = Sammy;
 
 /***/ },
-/* 3 */,
-/* 4 */
+
+/***/ 194:
 /***/ function(module, exports) {
 
 	module.exports = jQuery;
 
 /***/ },
-/* 5 */
+
+/***/ 195:
 /***/ function(module, exports) {
 
 	module.exports = Mustache;
 
 /***/ },
-/* 6 */
+
+/***/ 196:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	__webpack_require__(7);
-	__webpack_require__(10);
+	__webpack_require__(197);
+	__webpack_require__(200);
 
-	__webpack_require__(12);
+	__webpack_require__(202);
 
 /***/ },
-/* 7 */
+
+/***/ 197:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	__webpack_require__(8);
-	__webpack_require__(9);
+	__webpack_require__(198);
+	__webpack_require__(199);
 
 /***/ },
-/* 8 */
+
+/***/ 198:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var Sammy = __webpack_require__(2);
+	var Sammy = __webpack_require__(192);
 
 	Sammy('#main', function () {
 	  console.log('hello');
@@ -83,75 +91,147 @@ webpackJsonp([0],[
 	});
 
 /***/ },
-/* 9 */
+
+/***/ 199:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
-	var Sammy = __webpack_require__(2);
-	__webpack_require__(3);
+	var Sammy = __webpack_require__(192);
+	__webpack_require__(193);
 
 	Sammy('#main', function () {
-	  this.use('mustache', 'ms');
+	  this.use('Mustache', 'ms');
 
 	  this.get('#/user/property', function (context) {
 	    console.log('property');
 	    context.partial('/templates/user/property.ms').then(function () {
-	      propertyBindEvent();
+	      getPropertyList(0, 5);
 	    });
 	  });
 	});
 
+	function getPropertyList(pageNum, pageLength) {
+	  $.ajax({
+	    url: '/api/user/property/others',
+	    type: 'GET',
+	    data: {
+	      page: pageNum,
+	      length: pageLength
+	    },
+	    error: function error(_error) {
+	      console.error('ajax property get other error');
+	      return;
+	    },
+	    success: function success(data) {
+	      console.log(data);
+	      buildPropertyCard(data.data);
+	    }
+	  });
+	}
+
+	function postPropertyRepair(propertyId, type, remark) {
+	  $.ajax({
+	    url: '/api/user/repair/create',
+	    type: 'POST',
+	    data: {
+	      property_id: propertyId,
+	      type: type,
+	      remark: remark
+	    },
+	    error: function error(_error2) {
+	      console.error('ajax property repair create error');
+	      return;
+	    },
+	    success: function success(data) {
+	      console.log(data);
+	    }
+	  });
+	}
+
+	function buildPropertyCard(propertyData) {
+	  var i;
+	  for (i = 0; i < propertyData.length; i++) {
+	    var status = propertyData[i].status.id; //3: normal, 4:maintenance
+	    var color = status == 3 ? 'teal' : status == 4 ? 'red' : 'blue';
+	    var divCard = '<div class="card">';
+	    var divCardContent = '<div class="row card-content" ' + 'data-name="' + propertyData[i].name + '"' + 'data-propertyid="' + propertyData[i].id + '"' + 'data-describe="' + propertyData[i].describe + '">';
+	    var spanName = '<span class="col s4 center-align">' + propertyData[i].name + '</span>';
+	    var spanStatus = '<span class="col s4 center-align" style="color:' + color + '">' + propertyData[i].status.name + '</span>';
+	    var spanBtn = '<span class="col s4 center-align"><a class="waves-effect waves-light btn modal-trigger ' + (status == 4 ? 'disabled' : '') + '" data-modal_target="property_modal"><i class="material-icons left">build</i>報修/清理' + '</a></span></div></div>';
+	    $('#property_system_content').append(divCard + divCardContent + spanName + spanStatus + spanBtn);
+	  }
+	  propertyBindEvent();
+	}
+
 	function propertyBindEvent() {
-	  $('#property_container #property_system').click(function () {
+	  var $propertyContainer = $('#property_container');
+	  $propertyContainer.find('#property_system').on('click', function (event) {
 	    $('#property_system').addClass('purple darken-4').css('color', 'white');
 	    $('#property_history').removeClass('purple darken-4').addClass('white').css('color', 'black');
 	    $('#property_system_content').css('display', 'block');
 	    $('#property_history_content').css('display', 'none');
 	  });
 
-	  $('#property_container #property_history').click(function () {
+	  $propertyContainer.find('#property_history').on('click', function (event) {
 	    $('#property_system').removeClass('purple darken-4').addClass('white').css('color', 'black');
 	    $('#property_history').addClass('purple darken-4').css('color', 'white');
 	    $('#property_system_content').css('display', 'none');
 	    $('#property_history_content').css('display', 'block');
 	  });
 
-	  var modalTarget;
-	  $('#property_container').find('.modal-trigger').on('click', function (event) {
+	  var $modalTarget;
+	  $propertyContainer.find('.modal-trigger').on('click', function (event) {
+	    if ($(this).hasClass('disabled')) {
+	      return;
+	    }
+
 	    $('#materialize-lean-overlay-30').css('display', 'block');
-	    modalTarget = $(this).data('modal_target');
-	    $('#' + modalTarget).fadeIn();
+	    $modalTarget = $('#' + $(this).data('modal_target'));
+	    $modalTarget.fadeIn();
+
+	    var ele = $(this).parent().parent();
+	    $modalTarget.find('.modal-content h4').html(ele.data('name'));
+	    $modalTarget.find('p').html(ele.data('describe'));
+	    $modalTarget.data('propertyid', ele.data('propertyid'));
 	  });
 
-	  $('#property_container').find('.modal-close, #materialize-lean-overlay-30').on('click', function (event) {
+	  $propertyContainer.find('.modal-close, #materialize-lean-overlay-30').on('click', function (event) {
 	    $('#materialize-lean-overlay-30').css('display', 'none');
-	    $('#' + modalTarget).fadeOut();
+	    $modalTarget.fadeOut();
+	  });
+
+	  $propertyContainer.find('#property_repair_submit').on('click', function (event) {
+	    var type = $modalTarget.find('input[type="radio"]:checked').val();
+	    var propertyid = $modalTarget.data('propertyid');
+	    var remark = $modalTarget.find('#reason').val();
+	    postPropertyRepair(propertyid, type, remark);
 	  });
 	}
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(194)))
 
 /***/ },
-/* 10 */
+
+/***/ 200:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	__webpack_require__(11);
+	__webpack_require__(201);
 
 /***/ },
-/* 11 */
+
+/***/ 201:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
-	var Sammy = __webpack_require__(2);
+	var Sammy = __webpack_require__(192);
 
 	Sammy('#main', function () {
 	  this.use('Mustache', 'ms');
 
 	  this.get('#/admin/examine', function (context) {
-	    console.log('examine');
 	    context.list = [{
 	      id: 1,
 	      username: 'foo',
@@ -206,15 +286,16 @@ webpackJsonp([0],[
 	    });
 	  });
 	});
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(194)))
 
 /***/ },
-/* 12 */
+
+/***/ 202:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var Sammy = __webpack_require__(2);
+	var Sammy = __webpack_require__(192);
 
 	Sammy('#main', function () {
 	  this.get('#/', function (context) {
@@ -223,4 +304,5 @@ webpackJsonp([0],[
 	});
 
 /***/ }
-]);
+
+});
--- resources/assets/javascript/routes/user/property.js
+++ resources/assets/javascript/routes/user/property.js
@@ -7,35 +7,110 @@ Sammy('#main', function() {
   this.get('#/user/property', function(context) {
     console.log('property');
     context.partial('/templates/user/property.ms').then(function() {
-      propertyBindEvent();
+      getPropertyList(0, 5);
     });
   });
 });
 
+function getPropertyList(pageNum, pageLength) {
+  $.ajax({
+    url: '/api/user/property/others',
+    type: 'GET',
+    data: {
+      page: pageNum,
+      length: pageLength
+    },
+    error: function(error) {
+      console.error('ajax property get other error');
+      return;
+    },
+    success: function(data) {
+      console.log(data);
+      buildPropertyCard(data.data);
+    }
+  });
+}
+
+function postPropertyRepair(propertyId, type, remark) {
+  $.ajax({
+    url: '/api/user/repair/create',
+    type: 'POST',
+    data: {
+      property_id: propertyId,
+      type: type,
+      remark: remark
+    },
+    error: function(error) {
+      console.error('ajax property repair create error');
+      return;
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
+
+function buildPropertyCard(propertyData) {
+  var i;
+  for(i = 0; i < propertyData.length; i++) {
+    var status = propertyData[i].status.id;//3: normal, 4:maintenance
+    var color = status == 3 ? 'teal' : (status == 4 ? 'red' : 'blue');
+    var divCard = '<div class="card">';
+    var divCardContent = '<div class="row card-content" ' +
+                          'data-name="' + propertyData[i].name + '"' +
+                          'data-propertyid="' + propertyData[i].id + '"' +
+                          'data-describe="' + propertyData[i].describe + '">';
+    var spanName = '<span class="col s4 center-align">' + propertyData[i].name + '</span>';
+    var spanStatus = '<span class="col s4 center-align" style="color:' + color + '">' +
+                      propertyData[i].status.name + '</span>';
+    var spanBtn = '<span class="col s4 center-align"><a class="waves-effect waves-light btn modal-trigger ' +
+                  (status == 4 ? 'disabled' : '') +
+                  '" data-modal_target="property_modal"><i class="material-icons left">build</i>報修/清理' +
+                  '</a></span></div></div>';
+    $('#property_system_content').append(divCard + divCardContent + spanName + spanStatus + spanBtn);
+  }
+  propertyBindEvent();
+}
+
 function propertyBindEvent() {
-  $('#property_container #property_system').click(function() {
+  var $propertyContainer = $('#property_container');
+  $propertyContainer.find('#property_system').on('click', function(event) {
     $('#property_system').addClass('purple darken-4').css('color', 'white');
     $('#property_history').removeClass('purple darken-4').addClass('white').css('color', 'black');
     $('#property_system_content').css('display', 'block');
     $('#property_history_content').css('display', 'none');
   });
 
-  $('#property_container #property_history').click(function() {
+  $propertyContainer.find('#property_history').on('click', function(event) {
     $('#property_system').removeClass('purple darken-4').addClass('white').css('color', 'black');
     $('#property_history').addClass('purple darken-4').css('color', 'white');
     $('#property_system_content').css('display', 'none');
     $('#property_history_content').css('display', 'block');
   });
 
-  var modalTarget;
-  $('#property_container').find('.modal-trigger').on('click', function(event) {
+  var $modalTarget;
+  $propertyContainer.find('.modal-trigger').on('click', function(event) {
+    if($(this).hasClass('disabled')) { return; }
+
     $('#materialize-lean-overlay-30').css('display', 'block');
-    modalTarget = $(this).data('modal_target');
-    $('#' + modalTarget).fadeIn();
+    $modalTarget = $('#' + $(this).data('modal_target'));
+    $modalTarget.fadeIn();
+
+    var ele = $(this).parent().parent();
+    $modalTarget.find('.modal-content h4').html(ele.data('name'));
+    $modalTarget.find('p').html(ele.data('describe'));
+    $modalTarget.data('propertyid', ele.data('propertyid'));
   });
 
-  $('#property_container').find('.modal-close, #materialize-lean-overlay-30').on('click', function(event) {
+  $propertyContainer.find('.modal-close, #materialize-lean-overlay-30').on('click', function(event) {
     $('#materialize-lean-overlay-30').css('display', 'none');
-    $('#' + modalTarget).fadeOut();
+    $modalTarget.fadeOut();
+  });
+
+  $propertyContainer.find('#property_repair_submit').on('click', function(event) {
+    var type = $modalTarget.find('input[type="radio"]:checked').val();
+    var propertyid = $modalTarget.data('propertyid');
+    var remark = $modalTarget.find('#reason').val();
+    postPropertyRepair(propertyid, type, remark);
   });
 }
