229f4bffcada298181814eed3535b29c390cc90f
BePsvPT <freedom@csie.io>
2015-12-20 23:41:34 +0800

    Use helper function to generate asset urls

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
+        <meta id="csrf-token" name="csrf-token" content="{{ csrf_token() }}">
         <title>系務系統</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700">
         <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosanstc.css">
-        <link rel="stylesheet" href="/styles.css">
-        <meta id="csrf-token" name="csrf-token" content="{{csrf_token()}}">
+        <link rel="stylesheet" href="{{ asset('styles.css') }}">
     </head>
     <body>
         <div id="main"></div>
@@ -17,7 +17,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.js" defer> </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.6/sammy.js" defer></script>
-        <script src="/vendors.js" defer></script>
-        <script src="/bundle.js" defer></script>
+        <script src="{{ asset('vendors.js') }}" defer></script>
+        <script src="{{ asset('bundle.js') }}" defer></script>
     </body>
 </html>
