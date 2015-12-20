498768a9aad4e0c29a9617d7f763d560b6716965
DanSnow <dododavid006@gmail.com>
2015-12-20 20:43:37 +0800

    Add lodash

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,6 +13,7 @@
     <body>
         <div id="main"></div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.js" defer> </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.6/sammy.js" defer></script>
