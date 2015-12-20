29b8e17e1eec363e69861cac85da24ca74791d57
BePsvPT <freedom@csie.io>
2015-12-20 23:49:29 +0800

    Fix routes' name

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,22 +23,22 @@ $router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $rout
         $router->get('oauth/{verify}', ['as' => 'oauth.verify', 'uses' => 'OAuthController@verifyToken']);
     });
 
-    $router->group(['prefix' => 'user', 'as' => 'api.user'], function (Router $router) {
+    $router->group(['prefix' => 'user'], function (Router $router) {
         $router->get('login', 'User\UserAuthController@login');
 
-        $router->group(['prefix' => 'property', 'as' => 'api.user.property'], function (Router $router) {
+        $router->group(['prefix' => 'property'], function (Router $router) {
             $router->get('others', ['as' => 'api.user.other.list', 'uses' => 'User\PropertyController@index']);
             $router->get('classrooms', ['as' => 'api.user.class.list', 'uses' => 'User\PropertyController@indexClassroom']);
             $router->get('others/{id}', ['as' => 'api.user.other.detail', 'uses' => 'User\PropertyController@show']);
         });
 
-        $router->group(['prefix' => 'repair', 'as' => 'api.user.repair'], function (Router $router) {
+        $router->group(['prefix' => 'repair'], function (Router $router) {
             $router->get('/', ['as' => 'api.user.repair.list', 'uses' => 'User\RepairController@index']);
             $router->post('create', ['as' => 'api.user.repair.create', 'uses' => 'User\RepairController@store']);
             $router->delete('delete/{id}', ['as' => 'api.user.repair.delete', 'uses' => 'User\RepairController@delete']);
         });
 
-        $router->group(['prefix' => 'loan', 'as' => 'api.user.loan'], function (Router $router) {
+        $router->group(['prefix' => 'loan'], function (Router $router) {
             $router->get('others', ['as' => 'api.user.loan.other.list', 'uses' => 'User\LoanController@index']);
             $router->get('classrooms', ['as' => 'api.user.loan.class.list', 'uses' => 'User\LoanController@indexClassroomBorrow']);
             $router->post('create', ['as' => 'api.user.loan.class.create', 'uses' => 'User\LoanController@storeClassroomBorrow']);
@@ -46,10 +46,10 @@ $router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $rout
         });
     });
 
-    $router->group(['prefix' => 'manager', 'as' => 'api.manager'], function (Router $router) {
-        $router->get('login', 'Api\Manager\ManagerAuthController@login');
+    $router->group(['prefix' => 'manager'], function (Router $router) {
+        $router->get('login', 'Manager\ManagerAuthController@login');
 
-        $router->group(['prefix' => 'property', 'as' => 'api.man.property'], function (Router $router) {
+        $router->group(['prefix' => 'property'], function (Router $router) {
             $router->get('others', ['as' => 'api.man.other.list', 'uses' => 'Manager\PropertyController@index']);
             $router->get('classrooms', ['as' => 'api.man.class.list', 'uses' => 'Manager\PropertyController@indexClassroom']);
             $router->get('others/{id}', ['as' => 'api.man.other.detail', 'uses' => 'Manager\PropertyController@show']);
@@ -58,12 +58,12 @@ $router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $rout
             $router->delete('delete/{id}', ['as' => 'api.man.pro.delete', 'uses' => 'Manager\PropertyController@delete']);
         });
 
-        $router->group(['prefix' => 'repair', 'as' => 'api.man.repair'], function (Router $router) {
+        $router->group(['prefix' => 'repair'], function (Router $router) {
             $router->get('/', ['as' => 'api.man.repair.list', 'uses' => 'Manager\RepairController@index']);
             $router->put('/', ['as' => 'api.man.repair.update', 'uses' => 'Manager\RepairController@update']);
         });
 
-        $router->group(['prefix' => 'loan', 'as' => 'api.man.loan'], function (Router $router) {
+        $router->group(['prefix' => 'loan'], function (Router $router) {
             $router->get('others', ['as' => 'api.man.loan.other.list', 'uses' => 'Manager\LoanController@index']);
             $router->get('classrooms', ['as' => 'api.man.loan.class.list', 'uses' => 'Manager\LoanController@indexClassroomBorrow']);
             $router->post('other-create', ['as' => 'api.user.loan.other.create', 'uses' => 'Manager\LoanController@store']);
@@ -71,12 +71,12 @@ $router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $rout
             $router->put('class-verify/{id}', ['as' => 'api.user.loan.class.verify', 'uses' => 'Manager\LoanController@updateClassroomBorrow']);
         });
 
-        $router->group(['prefix' => 'setting', 'as' => 'api.man.setting'], function (Router $router) {
+        $router->group(['prefix' => 'setting'], function (Router $router) {
             $router->get('/', ['as' => 'api.man.setting.get', 'uses' => 'Manager\LoanController@getClassroomBorrowInfo']);
             $router->put('/', ['as' => 'api.man.setting.set', 'uses' => 'Manager\LoanController@setClassroomBorrowInfo']);
         });
 
-        $router->group(['prefix' => 'user', 'as' => 'api.man.user'], function (Router $router) {
+        $router->group(['prefix' => 'user'], function (Router $router) {
             $router->get('/', ['as' => 'api.man.user.list', 'uses' => 'Manager\UserController@index']);
             $router->get('/{id}', ['as' => 'api.man.user.detail', 'uses' => 'Manager\UserController@show']);
             $router->post('/', ['as' => 'api.man.user.create', 'uses' => 'Manager\UserController@create']);
