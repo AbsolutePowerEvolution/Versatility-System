05e47ebb87916e7d0ee77eb3f12ef103a995c47b
thumbd12856@gmail.com <thumbd12856@gmail.com>
2015-12-21 18:36:46 +0800

    user property history log v1

--- public/bundle.js
+++ public/bundle.js
@@ -100,6 +100,28 @@ webpackJsonp([0],{
 	var Sammy = __webpack_require__(192);
 	var client = __webpack_require__(200);
 	__webpack_require__(193);
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1; //January is 0!
+	var yyyy = today.getFullYear();
+	if (dd < 10) {
+	  dd = '0' + dd;
+	}
+	if (mm < 10) {
+	  mm = '0' + mm;
+	}
+	today = yyyy + '/' + mm + '/' + dd;
+
+	Object.size = function (obj) {
+	  var size = 0;
+	  var key;
+	  for (key in obj) {
+	    if (obj.hasOwnProperty(key)) {
+	      size++;
+	    }
+	  }
+	  return size;
+	};
 
 	Sammy('#main', function () {
 	  this.use('Mustache', 'ms');
@@ -108,6 +130,7 @@ webpackJsonp([0],{
 	    console.log('property');
 	    context.partial('/templates/user/property.ms').then(function () {
 	      getPropertyList(1, 5, 1);
+	      getRepairList(1, 10000);
 	    });
 	  });
 	});
@@ -132,20 +155,64 @@ webpackJsonp([0],{
 	  });
 	}
 
-	function postPropertyRepair(propertyId, type, remark) {
-	  client({
-	    path: 'user/repair/create',
-	    method: 'POST',
-	    params: {
-	      property_id: propertyId,
-	      type: type,
-	      remark: remark
+	function buildPropertyCard(propertyData) {
+	  var i;
+	  $('#property_system_content').find('.propertyContent').remove();
+	  for (i = 0; i < propertyData.length; i++) {
+	    var status = propertyData[i].status.id; //3: normal, 4:maintenance
+	    var color = status == 3 ? 'teal' : status == 4 ? 'red' : 'blue';
+	    var divCard = '<div class="card propertyContent">';
+	    var divCardContent = '<div class="row card-content" ' + 'data-name="' + propertyData[i].name + '"' + 'data-propertyid="' + propertyData[i].id + '"' + 'data-describe="' + propertyData[i].describe + '">';
+	    var spanName = '<span class="col s4 center-align">' + propertyData[i].name + '</span>';
+	    var spanStatus = '<span class="col s4 center-align" style="color:' + color + '">' + propertyData[i].status.name + '</span>';
+	    var spanBtn = '<span class="col s4 center-align"><a class="waves-effect waves-light btn modal-trigger ' + (status == 4 ? 'disabled' : '') + '" data-modal_target="property_modal"><i class="material-icons left">build</i>報修/清理' + '</a></span></div></div>';
+	    $('#property_system_content').append(divCard + divCardContent + spanName + spanStatus + spanBtn);
+	  }
+	  propertyBindEvent();
+	}
+
+	function propertyBindEvent() {
+	  var $propertyContainer = $('#property_container');
+	  $propertyContainer.find('#property_system').on('click', function (event) {
+	    $propertyContainer.find('#property_system').addClass('purple darken-4').css('color', 'white');
+	    $propertyContainer.find('#property_history').removeClass('purple darken-4').addClass('white').css('color', 'black');
+	    $propertyContainer.find('.property_system').css('display', 'block');
+	    $propertyContainer.find('#property_history_content').css('display', 'none');
+	  });
+
+	  $propertyContainer.find('#property_history').on('click', function (event) {
+	    $propertyContainer.find('#property_system').removeClass('purple darken-4').addClass('white').css('color', 'black');
+	    $propertyContainer.find('#property_history').addClass('purple darken-4').css('color', 'white');
+	    $propertyContainer.find('.property_system').css('display', 'none');
+	    $propertyContainer.find('#property_history_content').css('display', 'block');
+	  });
+
+	  var $modalTarget;
+	  $propertyContainer.find('.modal-trigger').on('click', function (event) {
+	    if ($(this).hasClass('disabled')) {
+	      return;
 	    }
-	  }).then(function (response) {
-	    console.log(response);
-	    //location.reload();
-	  }).catch(function (response) {
-	    return console.log(response);
+
+	    $('#materialize-lean-overlay-30').css('display', 'block');
+	    $modalTarget = $('#' + $(this).data('modal_target'));
+	    $modalTarget.fadeIn();
+
+	    var ele = $(this).parent().parent();
+	    $modalTarget.find('.modal-content h4').html(ele.data('name'));
+	    $modalTarget.find('p').html(ele.data('describe'));
+	    $modalTarget.data('propertyid', ele.data('propertyid'));
+	  });
+
+	  $propertyContainer.find('.modal-close, #materialize-lean-overlay-30').on('click', function (event) {
+	    $('#materialize-lean-overlay-30').css('display', 'none');
+	    $modalTarget.fadeOut();
+	  });
+
+	  $propertyContainer.find('#property_repair_submit').on('click', function (event) {
+	    var type = $modalTarget.find('input[type="radio"]:checked').val();
+	    var propertyid = $modalTarget.data('propertyid');
+	    var remark = $modalTarget.find('#reason').val();
+	    postPropertyRepair(propertyid, type, remark);
 	  });
 	}
 
@@ -153,12 +220,12 @@ webpackJsonp([0],{
 	  console.log('propertyTotalPage:' + propertyTotalPage);
 	  var i;
 	  var page = '<li class="active waves-effect"><a data-pageNum="1">1</a></li>';
-	  for (i = 1; i < propertyTotalPage / 5 - 1; i++) {
+	  for (i = 1; i < propertyTotalPage / 5; i++) {
 	    page += '<li class="waves-effect"><a data-pageNum="' + (i + 1) + '">' + (i + 1) + '</a></li>  ';
 	  }
 	  page += '<li class="waves-effect"><a data-pageNum="next"><i class="material-icons">chevron_right</i></a></li>';
 	  $('#property_container').find('.pagination').append(page);
-	  pageEvent(propertyTotalPage / 5 - 1);
+	  pageEvent(propertyTotalPage / 5);
 	}
 
 	function pageEvent(limit) {
@@ -189,65 +256,188 @@ webpackJsonp([0],{
 	  });
 	}
 
-	function buildPropertyCard(propertyData) {
-	  var i;
-	  $('#property_system_content').find('.propertyContent').remove();
-	  for (i = 0; i < propertyData.length; i++) {
-	    var status = propertyData[i].status.id; //3: normal, 4:maintenance
-	    var color = status == 3 ? 'teal' : status == 4 ? 'red' : 'blue';
-	    var divCard = '<div class="card propertyContent">';
-	    var divCardContent = '<div class="row card-content" ' + 'data-name="' + propertyData[i].name + '"' + 'data-propertyid="' + propertyData[i].id + '"' + 'data-describe="' + propertyData[i].describe + '">';
-	    var spanName = '<span class="col s4 center-align">' + propertyData[i].name + '</span>';
-	    var spanStatus = '<span class="col s4 center-align" style="color:' + color + '">' + propertyData[i].status.name + '</span>';
-	    var spanBtn = '<span class="col s4 center-align"><a class="waves-effect waves-light btn modal-trigger ' + (status == 4 ? 'disabled' : '') + '" data-modal_target="property_modal"><i class="material-icons left">build</i>報修/清理' + '</a></span></div></div>';
-	    $('#property_system_content').append(divCard + divCardContent + spanName + spanStatus + spanBtn);
-	  }
-	  propertyBindEvent();
+	function postPropertyRepair(propertyId, type, remark) {
+	  client({
+	    path: 'user/repair/create',
+	    method: 'POST',
+	    params: {
+	      property_id: propertyId,
+	      type: type,
+	      remark: remark
+	    }
+	  }).then(function (response) {
+	    console.log(response);
+	    //location.reload();
+	  }).catch(function (response) {
+	    return console.log(response);
+	  });
 	}
 
-	function propertyBindEvent() {
-	  var $propertyContainer = $('#property_container');
-	  $propertyContainer.find('#property_system').on('click', function (event) {
-	    $('#property_system').addClass('purple darken-4').css('color', 'white');
-	    $('#property_history').removeClass('purple darken-4').addClass('white').css('color', 'black');
-	    $('#property_system_content').css('display', 'block');
-	    $('#property_history_content').css('display', 'none');
-	  });
-
-	  $propertyContainer.find('#property_history').on('click', function (event) {
-	    $('#property_system').removeClass('purple darken-4').addClass('white').css('color', 'black');
-	    $('#property_history').addClass('purple darken-4').css('color', 'white');
-	    $('#property_system_content').css('display', 'none');
-	    $('#property_history_content').css('display', 'block');
-	  });
-
-	  var $modalTarget;
-	  $propertyContainer.find('.modal-trigger').on('click', function (event) {
-	    if ($(this).hasClass('disabled')) {
-	      return;
+	function getRepairList(pageNum, pageLength) {
+	  var debugData = {
+	    0: {
+	      id: 1,
+	      remark: 'reason',
+	      create_at: '2014/12/12',
+	      type: {
+	        id: 5,
+	        name: 'cleanup'
+	      },
+	      status: {
+	        id: 1,
+	        name: 'cleaned'
+	      },
+	      property: {
+	        name: 'remote controller 101'
+	      }
+	    },
+	    1: {
+	      id: 2,
+	      remark: 'reason2',
+	      create_at: '2015/12/20',
+	      type: {
+	        id: 6,
+	        name: 'maintain'
+	      },
+	      status: {
+	        id: 1,
+	        name: 'finished'
+	      },
+	      property: {
+	        name: 'remote controller 206'
+	      }
 	    }
+	  };
+	  var sortData = [];
+	  var i;
+	  var dataLength = Object.size(debugData);
+	  for (i = 0; i < dataLength; i++) {
+	    sortData[i] = [];
+	    sortData[i][0] = i;
+	    sortData[i][1] = 0;
+	    sortData[i][2] = debugData[i].create_at;
+	  }
+	  getPropertyLoan(1, 10000, sortData, debugData);
+	  //buildRepairHistoryCard(debugData);
+	  /*client({
+	    path: 'user/repair',
+	    method: 'GET',
+	    params: {
+	      page: pageNum,
+	      length: pageLength
+	    }
+	  }).then(function(response) {
+	    console.log(response);
+	    buildRepairCard(response.entity.data);
+	  }).catch((response) => console.log(response));*/
+	}
 
-	    $('#materialize-lean-overlay-30').css('display', 'block');
-	    $modalTarget = $('#' + $(this).data('modal_target'));
-	    $modalTarget.fadeIn();
-
-	    var ele = $(this).parent().parent();
-	    $modalTarget.find('.modal-content h4').html(ele.data('name'));
-	    $modalTarget.find('p').html(ele.data('describe'));
-	    $modalTarget.data('propertyid', ele.data('propertyid'));
+	function getPropertyLoan(pageNum, pageLength, sortData, repairData) {
+	  var debugData = {
+	    0: {
+	      id: 1,
+	      property_name: 'air condition remote controller 001',
+	      data_began_at: '2015/12/11',
+	      data_ended_at: '2015/12/11  ',
+	      time_began_at: '10:00',
+	      time_ended_at: '12:00',
+	      remark: 'weather is hot',
+	      type: {
+	        id: 5,
+	        name: 'cleanup'
+	      },
+	      status: {
+	        id: 1,
+	        name: 'refused'
+	      }
+	    },
+	    1: {
+	      id: 2,
+	      property_name: 'air condition remote controller 206',
+	      data_began_at: '3015/03/19',
+	      data_ended_at: '3015/03/19',
+	      time_began_at: '14:00',
+	      time_ended_at: '16:00',
+	      remark: 'weather is very hot',
+	      type: {
+	        id: 6,
+	        name: 'maintain'
+	      },
+	      status: {
+	        id: 2,
+	        name: 'can be loaned'
+	      }
+	    }
+	  };
+	  var i;
+	  var j;
+	  var totalLength = Object.size(sortData);
+	  var dataLength = Object.size(debugData);
+	  for (i = totalLength, j = 0; i < totalLength + dataLength; i++, j++) {
+	    sortData[i] = [];
+	    sortData[i][0] = j;
+	    sortData[i][1] = 1;
+	    sortData[i][2] = debugData[j].data_began_at;
+	  }
+	  //console.log('before sort: totalData:' + totalData);
+	  sortData.sort(function (a, b) {
+	    return new Date(a[2]) < new Date(b[2]);
 	  });
+	  //console.log('after sort: totalData:' + totalData);
+	  buildHistoryCard(sortData, repairData, debugData);
 
-	  $propertyContainer.find('.modal-close, #materialize-lean-overlay-30').on('click', function (event) {
-	    $('#materialize-lean-overlay-30').css('display', 'none');
-	    $modalTarget.fadeOut();
-	  });
+	  //buildPropertyHistoryCard(debugData);
+	  //buildRepairHistoryCard(debugData);
+	  /*client({
+	   path: 'user/loan/others',
+	   method: 'GET',
+	   params: {
+	   page: pageNum,
+	   length: pageLength
+	   }
+	   }).then(function(response) {
+	   console.log(response);
+	   }).catch((response) => console.log(response));*/
+	}
 
-	  $propertyContainer.find('#property_repair_submit').on('click', function (event) {
-	    var type = $modalTarget.find('input[type="radio"]:checked').val();
-	    var propertyid = $modalTarget.data('propertyid');
-	    var remark = $modalTarget.find('#reason').val();
-	    postPropertyRepair(propertyid, type, remark);
-	  });
+	function buildHistoryCard(sortData, repairData, propertyData) {
+	  var i;
+	  var dataLength = Object.size(sortData);
+	  for (i = 0; i < dataLength; i++) {
+	    if (sortData[i][1] == 0) {
+	      //repair
+	      //console.log('repair:' , repairData[sortData[i][0]]);
+	      buildRepairHistoryCard(repairData[sortData[i][0]]);
+	    } else if (sortData[i][1] == 1) {
+	      //property
+	      //console.log('property:' , propertyData[sortData[i][0]]);
+	      buildPropertyHistoryCard(propertyData[sortData[i][0]]);
+	    }
+	  }
+	}
+
+	function buildRepairHistoryCard(data) {
+	  console.log('Repair history:' + data);
+	  var color = today > data.create_at ? 'teal' : 'red';
+	  var divCard = '<div class="card waves-effect">';
+	  var divCardContent = '<div class="row card-content">';
+	  var spanName = '<span class="col s4 center-align">' + data.property.name + '</span>';
+	  var spanDate = '<span class="col s4 center-align " style="color:' + color + '">' + data.create_at + '</span>';
+	  var spanType = '<span class="col s4 center-align">' + data.type.name + '</span></div></div>';
+	  //console.log(divCard + divCardContent + spanName + spanDate + spanType);
+	  $('#property_history_content').append(divCard + divCardContent + spanName + spanDate + spanType);
+	}
+
+	function buildPropertyHistoryCard(data) {
+	  console.log('Property history:' + data);
+	  var color = today > data.data_began_at ? 'teal' : 'red';
+	  var divCard = '<div class="card waves-effect">';
+	  var divCardContent = '<div class="row card-content">';
+	  var spanName = '<span class="col s4 center-align">' + data.property_name + '</span>';
+	  var spanDate = '<span class="col s4 center-align" style="color:' + color + '">' + data.data_began_at + ' ' + data.time_began_at + ' - ' + data.data_ended_at + ' ' + data.time_ended_at + '</span>';
+	  var spanType = '<span class="col s4 center-align">' + data.status.name + '</span></div></div>';
+	  //console.log(divCard + divCardContent + spanName + spanDate + spanType);
+	  $('#property_history_content').append(divCard + divCardContent + spanName + spanDate + spanType);
 	}
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(194)))
 
