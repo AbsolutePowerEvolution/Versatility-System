da1b29e6e98ddb30d67b53cd924494dcdcc38794
DanSnow <dododavid006@gmail.com>
2015-12-22 21:23:02 +0800

    Update materialize; Fix font load

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,13 +8,14 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700">
         <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosanstc.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
         <link rel="stylesheet" href="{{ asset('styles.css') }}">
     </head>
     <body>
         <div id="main"></div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js" defer></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.4/js/materialize.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.js" defer> </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.6/sammy.js" defer></script>
         <script src="{{ asset('vendors.js') }}" defer></script>
