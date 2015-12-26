057f5e759dbb87629f0a275fea2303562e66459b
BePsvPT <freedom@csie.io>
2015-12-26 22:24:01 +0800

    Fix typo

--- tests/Api/Manager/Property/InvalidInputTest.php
+++ tests/Api/Manager/Property/InvalidInputTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class InvalidInputTest extends PropertyTest
 {
-    use DatabaseTransactions
+    use DatabaseTransactions;
 
     /**
      * @var string
