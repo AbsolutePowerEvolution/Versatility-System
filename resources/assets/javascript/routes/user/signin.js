cf7b7e396dafb622b66e4297a8279d5d1b22b8ab
DanSnow <dododavid006@gmail.com>
2015-12-28 16:31:59 +0800

    Cleanup

--- resources/assets/javascript/routes/user/signin.js
+++ resources/assets/javascript/routes/user/signin.js
@@ -5,12 +5,10 @@ Sammy('#main', function() {
   this.use('Mustache', 'ms');
 
   this.get('#/user/signin', function(context) {
-    console.log('signin');
     context.partial('/templates/user/signin.ms');
   });
 
   this.post('#/user/signin', function(context) {
-    console.log(context);
     console.log('Username: ' + context.params.username);
     console.log('Password: ' + context.params.password);
     return false;
