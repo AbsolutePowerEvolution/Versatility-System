dac58e055984d09499b032f86fdd1998cdd72501
DanSnow <dododavid006@gmail.com>
2015-12-28 13:19:03 +0800

    Disable source map in production mode

--- resources/tasks/sass.js
+++ resources/tasks/sass.js
@@ -5,15 +5,14 @@ var productionMode = process.env.NODE_ENV === 'production';
 
 gulp.task('sass', function() {
   return gulp.src('./resources/assets/sass/**/*.scss', {base: '.'})
-    .pipe($.sourcemaps.init())
+    .pipe($.if(!productionMode, $.sourcemaps.init()))
     .pipe($.sass({ importer: require('./helper/sass-importer') }))
-    .pipe($.sourcemaps.write())
+    .pipe($.if(!productionMode, $.sourcemaps.write()))
     .pipe($.order([
       'resources/assets/sass/app.css',
       'resources/assets/sass/**/*.css'
     ]))
     .pipe($.concat('styles.css'))
     .pipe($.if(productionMode, $.cssmin()))
-    .pipe($.if(productionMode, $.rename({suffix: '.min'})))
     .pipe(gulp.dest('./public/assets/css'));
 });
