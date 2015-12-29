7019262b8548dee5f54a719ccc212e5fe31e159a
BePsvPT <freedom@csie.io>
2015-12-29 15:43:32 +0800

    Update entrust

---
+++ app/Affair/Permission.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Affair;
+
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+}
---
+++ app/Affair/Role.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Affair;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+}
