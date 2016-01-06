b8a85e2c05653a931d0b420d1b7b193893d4a7bd
DanSnow <dododavid006@gmail.com>
2016-01-07 01:12:34 +0800

    Make babel-register loadable

--- resources/assets/javascript/lib/middleware/status-code.js
+++ resources/assets/javascript/lib/middleware/status-code.js
@@ -1,8 +1,8 @@
-import when from 'when';
+var when = require('when');
 
 // P2P-style unjustifed status code check
 
-export default (() => (response) => {
+module.exports = (() => (response) => {
   if(typeof response.body === 'object' && response.body.status) {
     if(response.body.status !== 0) {
       return when.reject(response);
--- resources/assets/javascript/routes/user/signin.js
+++ resources/assets/javascript/routes/user/signin.js
@@ -1,30 +1,35 @@
-var Sammy = require('sammy');
-var api = require('../../lib/fetch-plus');
+(function() {
+  'use strict';
+  var Sammy = require('sammy');
+  var api = require('../../lib/fetch-plus');
 
-Sammy('#main', function() {
-  this.get('#/user/signin', function(context) {
-    context.partial('/templates/user/signin.ms');
-  });
-
-  this.post('#/user/signin', function(context) {
-    let params = Object.assign({}, context.params);
-    api.add('auth/login', {
-      body: $.param(params)
-    }).then((data) => {
-      console.log(data);
-      if(data.status) {
-        if(data.is_manager) {
-          context.redirect('#/admin/examine');
-        } else if(data.is_student) {
-          context.redirect('#/user/loan');
-        }
-      } else {
-        Materialize.toast('帳號 or 密碼錯誤');
-      }
-    }).catch((response) => {
-      Materialize.toast('伺服器錯誤');
+  Sammy('#main', function(app) {
+    app.get('#/user/signin', function(context) {
+      context.partial('/templates/user/signin.ms')
+        .then(() => {
+          console.log('render');
+        });
     });
-    return false;
-  });
-});
 
+    app.post('#/user/signin', function(context) {
+      let params = Object.assign({}, context.params);
+      api.add('auth/login', {
+        body: $.param(params)
+      }).then((data) => {
+        console.log(data);
+        if(data.status) {
+          if(data.is_manager) {
+            context.redirect('#/admin/examine');
+          } else if(data.is_student) {
+            context.redirect('#/user/loan');
+          }
+        } else {
+          Materialize.toast('帳號 or 密碼錯誤');
+        }
+      }).catch((response) => {
+        Materialize.toast('伺服器錯誤');
+      });
+      return false;
+    });
+  });
+}).call(this);
