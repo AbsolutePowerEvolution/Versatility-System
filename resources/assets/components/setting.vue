a08eb561d47af50d7fa336830ac8d9b85fe0f46c
DanSnow <dododavid006@gmail.com>
2016-01-28 21:14:43 +0800

    Update

--- resources/assets/components/setting.vue
+++ resources/assets/components/setting.vue
@@ -3,22 +3,42 @@
   <div class="container">
     <form @submit="applySetting" action="#/admin/setting" method="PUT">
       <div class="input-field">
-        <input id="time_name" class="validate Setting-TimeName" type="text" name="time_name" v-model="time_name">
-        <label for="time_name" :class="{active: time_name}">名稱 (ex: 104上學期):</label>
+        <input id="time_name"
+          class="validate Setting-TimeName"
+          type="text" name="time_name"
+          v-model="time_name">
+        <label for="time_name"
+          :class="{active: time_name}">
+          名稱 (ex: 104上學期):
+        </label>
       </div>
-      <date-field :date.sync="began_date" name="began_date" class-name="Setting-BeganDate">
+      <date-field
+        :date.sync="began_date"
+         name="began_date"
+         class-name="Setting-BeganDate">
         開始時間：
       </date-field>
-      <date-field :date.sync="ended_date" name="ended_date" class-name="Setting-EndedDate">
+      <date-field
+        :date.sync="ended_date"
+        name="ended_date"
+        class-name="Setting-EndedDate">
         結束時間：
       </date-field>
-      <date-field :date.sync="stu_start" name="stu_start" class-name="Setting-StuStart">
+      <date-field
+        :date.sync="stu_start"
+        name="stu_start"
+        class-name="Setting-StuStart">
         學生借用開始時間：
       </date-field>
-      <date-field :date.sync="lab_start" name="lab_start" class-name="Setting-LabStart">
+      <date-field
+         :date.sync="lab_start"
+         name="lab_start"
+         class-name="Setting-LabStart">
         Lab 借用開始時間：
       </date-field>
-      <button id="apply-btn" type="submit" class="waves-effect waves-light btn">
+      <button id="apply-btn"
+        type="submit"
+        class="waves-effect waves-light btn">
         <i class="material-icons left">done</i>套用設定
       </button>
     </form>
