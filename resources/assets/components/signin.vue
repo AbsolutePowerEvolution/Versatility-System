878a88b64508780027f2b1682b8d6dc8def2f910
DanSnow <dododavid006@gmail.com>
2016-01-28 22:35:28 +0800

    Port signin page to vue.js

---
+++ resources/assets/components/signin.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="signin-card">
+    <div class="row">
+      <div class="col s12 m4 offset-m4">
+        <div class="card z-depth-3">
+          <div class="container">
+            <div class="row">
+              <div class="card-content black-text center-align">
+                <span class="card-title">資訊工程學系<br>系務系統</span>
+              </div>
+              <form @submit.prevent="doSignin">
+                <div class="input-field">
+                  <i class="material-icons prefix">account_box</i>
+                  <input type="text"
+                    id="username"
+                    name="username"
+                    v-model="username">
+                  <label for="username">Username</label>
+                </div>
+                <div class="input-field">
+                  <i class="material-icons prefix">lock</i>
+                  <input type="password"
+                    id="password"
+                    name="password"
+                    v-model="password">
+                  <label for="password">Password</label>
+                </div>
+                <div class="card-action center">
+                  <button class="waves-effect btn" type="button" @click.stop="doSignin">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import $ from 'jquery';
+  let userSym = Symbol('user');
+  let managerSym = Symbol('manager');
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        USER: userSym,
+        MANAGER: managerSym
+      };
+    },
+    methods: {
+      doSignin(event) {
+        let username = this.$data.username;
+        let password = this.$data.password;
+        let data = {
+          username,
+          password
+        };
+        this.$http.post('/api/auth/login', data)
+          .then((response) => {
+            console.log(response);
+            if(response.data.status) {
+              let sym = response.data.is_student ? userSym : managerSym;
+              this.$emit('login', sym);
+            } else {
+              this.$emit('fail');
+            }
+          }, (response) => {
+            console.log(response);
+            this.$emit('error', response);
+          });
+      }
+    }
+  }
+</script>
--- resources/assets/javascript/routes/user/signin.js
+++ resources/assets/javascript/routes/user/signin.js
@@ -1,35 +1,39 @@
-(function() {
-  'use strict';
-  var Sammy = require('sammy');
-  var api = require('../../lib/fetch-plus');
+var Sammy = require('sammy');
+var api = require('../../lib/fetch-plus');
 
-  Sammy('#main', function(app) {
-    app.get('#/user/signin', function(context) {
-      context.partial('/templates/user/signin.ms')
-        .then(() => {
-          console.log('render');
-        });
-    });
+import Signin from '../../../components/signin.vue';
 
-    app.post('#/user/signin', function(context) {
-      let params = Object.assign({}, context.params);
-      api.add('auth/login', {
-        body: $.param(params)
-      }).then((data) => {
-        console.log(data);
-        if(data.status) {
-          if(data.is_manager) {
-            context.redirect('#/admin/examine');
-          } else if(data.is_student) {
-            context.redirect('#/user/loan');
+Sammy('#main', function(app) {
+  app.get('#/user/signin', function(context) {
+    context.partial('/templates/vue.ms')
+      .then(() => {
+        let App = Vue.extend({
+          template: '<signin v-ref:signin></signin>',
+          components: { Signin },
+          compiled() {
+            let signin = this.$refs.signin;
+            signin.$on('login', (type) => {
+              if(type === signin.USER) {
+                context.redirect('#/user/loan');
+              } else {
+                context.redirect('#/admin/examine');
+              }
+            });
+
+            signin.$on('fail', () => {
+              Materialize.toast('帳號 or 密碼錯誤', 500);
+            });
+
+            signin.$on('error', () => {
+              Materialize.toast('伺服器錯誤', 500);
+            });
           }
-        } else {
-          Materialize.toast('帳號 or 密碼錯誤');
-        }
-      }).catch((response) => {
-        Materialize.toast('伺服器錯誤');
+        });
+
+        new Vue({
+          el: '#main',
+          components: { App }
+        });
       });
-      return false;
-    });
   });
-}).call(this);
+});
