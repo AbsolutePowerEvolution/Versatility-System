19a8756ece3533ad9f8ce69edfd679d3d838f0e1
DanSnow <dododavid006@gmail.com>
2016-02-01 22:16:39 +0800

    Split place for history display

---
+++ resources/assets/components/setting-form.vue
@@ -0,0 +1,87 @@
+<template>
+  <form @submit="applySetting" action="#/admin/setting" method="PUT">
+    <div class="input-field">
+      <input id="time-name"
+        class="validate Setting-TimeName"
+        type="text" name="time_name"
+        v-model="timeName">
+      <label for="time-name"
+        :class="{active: timeName}">
+        名稱 (ex: 104上學期):
+      </label>
+    </div>
+    <date-field
+      :date.sync="beganDate"
+       name="began_date"
+       class-name="Setting-BeganDate">
+      開始時間：
+    </date-field>
+    <date-field
+      :date.sync="endedDate"
+      name="ended_date"
+      class-name="Setting-EndedDate">
+      結束時間：
+    </date-field>
+    <date-field
+      :date.sync="stuStart"
+      name="stu_start"
+      class-name="Setting-StuStart">
+      學生借用開始時間：
+    </date-field>
+    <date-field
+       :date.sync="labStart"
+       name="lab_start"
+       class-name="Setting-LabStart">
+      Lab 借用開始時間：
+    </date-field>
+    <button id="apply-btn"
+      type="submit"
+      class="waves-effect waves-light btn">
+      <i class="material-icons left">done</i>套用設定
+    </button>
+  </form>
+</template>
+
+<script>
+  import DateField from './date-field.vue';
+  let mapData = {
+    timeName: 'time_name',
+    beganDate: 'began_date',
+    ended_date: 'ended_date',
+    stuStart: 'stu_start',
+    labStart: 'lab_start'
+  };
+  export default {
+    props: {
+      timeName: {
+        type: String,
+        default: ''
+      },
+      beganDate: {
+        type: String,
+        default: ''
+      },
+      endedDate: {
+        type: String,
+        default: ''
+      },
+      stuStart: {
+        type: String,
+        default: ''
+      },
+      labStart: {
+        type: String,
+        default: ''
+      },
+    },
+    methods: {
+      applySetting(event) {
+        event.preventDefault();
+        console.log(this.$data);
+      }
+    },
+    components: { DateField },
+    compiled() {
+    }
+  }
+</script>
--- resources/assets/components/setting.vue
+++ resources/assets/components/setting.vue
@@ -1,70 +1,25 @@
 <template>
   <admin-menu></admin-menu>
-  <div class="container">
-    <form @submit="applySetting" action="#/admin/setting" method="PUT">
-      <div class="input-field">
-        <input id="time_name"
-          class="validate Setting-TimeName"
-          type="text" name="time_name"
-          v-model="time_name">
-        <label for="time_name"
-          :class="{active: time_name}">
-          名稱 (ex: 104上學期):
-        </label>
-      </div>
-      <date-field
-        :date.sync="began_date"
-         name="began_date"
-         class-name="Setting-BeganDate">
-        開始時間：
-      </date-field>
-      <date-field
-        :date.sync="ended_date"
-        name="ended_date"
-        class-name="Setting-EndedDate">
-        結束時間：
-      </date-field>
-      <date-field
-        :date.sync="stu_start"
-        name="stu_start"
-        class-name="Setting-StuStart">
-        學生借用開始時間：
-      </date-field>
-      <date-field
-         :date.sync="lab_start"
-         name="lab_start"
-         class-name="Setting-LabStart">
-        Lab 借用開始時間：
-      </date-field>
-      <button id="apply-btn"
-        type="submit"
-        class="waves-effect waves-light btn">
-        <i class="material-icons left">done</i>套用設定
-      </button>
-    </form>
+  <div class="container Setting-Container">
+    <div class="m6 Setting-Column">
+    </div>
+    <div class="m6 Setting-Column">
+      <setting-form></setting-form>
+    </div>
   </div>
 </template>
 
 <script>
+  import when from 'when';
   import AdminMenu from './admin-menu.vue';
-  import DateField from './date-field.vue';
-  let data = {
-    time_name: '',
-    began_date: '',
-    ended_date: '',
-    stu_start: '',
-    lab_start: ''
-  }
+  import SettingForm from './setting-form.vue';
   export default {
-    data() {
-      return data;
-    },
-    methods: {
-      applySetting(event) {
-        event.preventDefault();
-        console.log(this.$data);
-      }
-    },
-    components: { AdminMenu, DateField }
+    components: { AdminMenu, SettingForm },
+    compiled() {
+      when(this.$http.get('manager/setting'))
+        .then((response) => {
+          console.log(response.data);
+        });
+    }
   }
 </script>
