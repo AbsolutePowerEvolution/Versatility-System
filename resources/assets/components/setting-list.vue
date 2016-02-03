4dcc0c8eba070d038c5f8661fe0dcc422c664357
DanSnow <dododavid006@gmail.com>
2016-02-03 21:50:05 +0800

    Fix property name

--- resources/assets/components/setting-list.vue
+++ resources/assets/components/setting-list.vue
@@ -5,7 +5,7 @@
       <li class="collection-item" v-for="setting in settings">
         <div>
           <div>{{setting.zone_name}}: {{setting.date_began_at}} ~ {{setting.date_ended_at}}</div>
-          <div>學生借用開始：{{setting.stu_start}}, Lab 借用開始：{{setting.lab_start}}</div>
+          <div>學生借用開始：{{setting.stu_date_began_at}}, Lab 借用開始：{{setting.lab_date_began_at}}</div>
         </div>
       </li>
     </ul>
